4b51a4e840c0d15689c4b8b789694466977678b0
Lars van den Nieuwendijk <larsvdnieuwendijk@t-mobilethuis.nl>
2021-09-08 09:59:56 +0200

    F1.2.02.L2

---
+++ deSpeelhal.py
@@ -0,0 +1,5 @@
+toegangsTicket = 7.45
+gameTijd = 0.37 * 9
+personen = 3
+
+print(str(toegangsTicket + gameTijd * personen))
---
+++ feestLunch.py
@@ -0,0 +1,5 @@
+croissants = 17 * 0.39
+stokbroden = 2 * 2.78
+kortingsbonnen = 3 * 0.50
+
+print(str(croissants + stokbroden - kortingsbonnen))
